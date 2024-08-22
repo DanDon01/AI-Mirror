30a42cab0d087d8152099550f3a9f7af64336631
Dan <33629090+DanDon01@users.noreply.github.com>
2024-08-22 23:22:40 +0100

    Create config.py
    Config file for all sensitive API Keys and ids.

---
+++ config.py
@@ -0,0 +1,28 @@
+CONFIG = {
+    'fitbit': {
+        'client_id': 'your_fitbit_client_id',
+        'client_secret': 'your_fitbit_client_secret',
+        'access_token': 'your_fitbit_access_token',
+        'refresh_token': 'your_fitbit_refresh_token'
+    },
+    'stocks': {
+        'api_key': 'your_stocks_api_key'
+    },
+    'weather': {
+        'api_key': 'your_weather_api_key'
+    },
+    'news': {
+        'api_key': 'your_news_api_key'
+    },
+    'calendar': {
+        'calendar_id': 'your_calendar_id',
+        'api_key': 'your_calendar_api_key'
+    },
+    'positions': {
+        'fitbit': (10, 50),
+        'stocks': (10, 150),
+        'weather': (400, 50),
+        'news': (10, 350),
+        'calendar': (400, 350)
+    }
+}
